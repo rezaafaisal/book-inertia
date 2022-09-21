ef313d2f1d387ea49438b0ba69c7125dbd29c8bc
Reza Faisal <rezha.faizal567@gmail.com>
2022-09-21 14:12:15 +0800

    client setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-import './bootstrap';
+import { createApp, h } from "vue"
+import { createInertiaApp } from "@inertiajs/inertia-vue3"
+import { head } from "lodash"
+
+createInertiaApp({
+    resolve: name => import(`./pages/${name}`),
+    setup({el, App, props, plugin}){
+        createApp({render: ()=> head(App, props)}).use(plugin).mount(el)
+    }
+})
