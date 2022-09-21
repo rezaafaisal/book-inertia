064571649735535fb4a268a2d9cdea336e7cb7dd
Reza Faisal <rezha.faizal567@gmail.com>
2022-09-21 14:57:06 +0800

    fix init config

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,8 @@
+
+<template>
+    <h2>aokaokaokw</h2>
+</template>
+
+<script setup>
+
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,9 @@
 import { createApp, h } from "vue"
 import { createInertiaApp } from "@inertiajs/inertia-vue3"
-import { head } from "lodash"
 
 createInertiaApp({
-    resolve: name => import(`./pages/${name}`),
+    resolve: name => import(`./Pages/${name}.vue`),
     setup({el, App, props, plugin}){
-        createApp({render: ()=> head(App, props)}).use(plugin).mount(el)
-    }
+        createApp({render: () => h(App, props)}).use(plugin).mount(el)
+    },
 })
